5a9964e8402c8bfb8032aaf8b5fc02717e716531
Ryan Alves <ra546038@gmail.com>
2023-12-13 11:50:44 -0300

    all

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express()
 app.use((request, response, next) => {
 
     response.header('Access-Control-Allow-Origin', '*')
-    response.header('Access-Control-Allow-Methods', '*')
+    response.header('Access-Control-Allow-Methods', 'GET, OPTIONS')
     response.header('Access-Control-Allow-Headers', '*')
     app.use(cors)
     next()
